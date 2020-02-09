23f77731414152ffab3c549179242af085bf63b4
mate00 <mami.ext@gmail.com>
2020-02-09 08:34:04 +0100

    Let's play with Spring Webflux!

---
+++ src/test/java/rnd/mate00/springwebflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package rnd.mate00.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
