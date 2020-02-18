6718091484f80f4ac39cc4a498d12b57afcece87
mate00 <mami.ext@gmail.com>
2020-02-18 06:40:14 +0100

    Corrected http headers.

--- src/main/java/rnd/mate00/springwebflux/leagues/LeagueCall.java
+++ src/main/java/rnd/mate00/springwebflux/leagues/LeagueCall.java
@@ -1,8 +1,8 @@
 package rnd.mate00.springwebflux.leagues;
 
-import com.sun.net.httpserver.Headers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,7 +15,7 @@ public class LeagueCall {
     private RestTemplate restTemplate;
 
     public void getLeagues() {
-        Headers headers = new Headers();
+        HttpHeaders headers = new HttpHeaders();
         headers.add("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
         headers.add("x-rapidapi-key", "577f914677mshe067ff566272b48p1e82c9jsn8e39d25f3dd0");
         HttpEntity entity = new HttpEntity(headers);
