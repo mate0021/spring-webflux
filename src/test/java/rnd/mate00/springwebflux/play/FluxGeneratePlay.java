199ee47570a7a8b4823b8e1c56359fcd49aa3a15
mate00 <mami.ext@gmail.com>
2020-03-09 07:07:57 +0100

    Play with generator functions.

--- src/test/java/rnd/mate00/springwebflux/play/FluxGeneratePlay.java
+++ src/test/java/rnd/mate00/springwebflux/play/FluxGeneratePlay.java
@@ -2,6 +2,11 @@ package rnd.mate00.springwebflux.play;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SynchronousSink;
+
+import java.util.concurrent.Callable;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 public class FluxGeneratePlay {
 
@@ -18,5 +23,50 @@ public class FluxGeneratePlay {
                 });
 
         generate.log().subscribe();
+    }
+
+    @Test(timeout = 1000)
+    public void generateUsingAnonymousClasses_ItWillGoEndlessly() {
+        Flux<String> strs = Flux.generate(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("initial callable");
+                return "A";
+            }
+        }, new BiFunction<String, SynchronousSink<String>, String>() {
+            @Override // we have to stop it somewhere here by ie. tSynschonousSink.complete()
+            public String apply(String s, SynchronousSink<String> tSynchronousSink) {
+                System.out.println("bifunc as a generator function");
+                tSynchronousSink.next(s + "A");
+                return s + "A";
+            }
+        }, new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("ending consumer");
+            }
+        });
+
+        strs.log().subscribe();
+    }
+
+    @Test
+    public void callCompleteInGeneratorFunction() {
+        Flux<String> strs = Flux.generate(
+                () -> "A",
+                new BiFunction<String, SynchronousSink<String>, String>() {
+                    @Override
+                    public String apply(String s, SynchronousSink<String> stringSynchronousSink) {
+                        stringSynchronousSink.next(s + "A");
+                        if (s.length() == 10) {
+                            stringSynchronousSink.complete();
+                        }
+                        return s + "B";
+                    }
+                },
+                System.out::println
+        );
+
+        strs.log().subscribe();
     }
 }
